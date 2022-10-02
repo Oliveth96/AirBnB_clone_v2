df607006e71d315d252a11d8bba57b989c1ae096
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-10-02 18:37:51 +0100

    A Fabric Script that distributes an archive to my servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
 # A Fabric Script that distributes an archive to my servers
-from fabric.api import local, env, run, put, sudo
+from datetime import datetime
+from fabric.api import *
+import shlex
+import os
 
 
-env.hosts = ['35.227.39.114', '35.231.236.123']
+env.hosts = ['35.231.33.237', '34.74.155.163']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """Deploy archive to servers"""
-    if archive_path:
-        archive_file = archive_path.split('/')[1]
-        archive_dir = archive_file.split('.')[0]
-        releases = '/data/web_static/releases/'
-        current = '/data/web_static/current'
+    """ Deploys """
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
 
-        try:
-            put(archive_path, '/tmp/')
-            run('mkdir -p {}{}'.format(releases, archive_dir))
-            run('tar -xzf /tmp/{} -C {}{}'.format(
-                archive_file, releases, archive_dir))
-            run('rm /tmp/{}'.format(archive_file))
-            run('mv {}{}/web_static/* {}{}'.format(
-                releases, archive_dir, releases, archive_dir))
-            run('rm -rf {}{}/web_static'.format(releases, archive_dir))
-            run('rm -rf {}'.format(current))
-            run('ln -s {}{} {}'.format(releases, archive_dir, current))
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
 
-            return True
-        except:
-            return False
-    else:
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    except:
         return False
