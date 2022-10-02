224d0b7b54bfe79056fca7c359d75ec2bbd887db
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-10-02 12:47:04 +0100

    A Bash script that sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+#A Bash script that sets up my
+#web servers for the deployment of web_static
+
+#Install Nginx
+apt install -y nginx
+
+#Create folders
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+
+#create a fake HTNL file
+echo "Fake HTML content ..." >/data/web_static/releases/test/index.html
+
+#Create a symbolic link
+sudo ln -sf "/data/web_static/releases/test" "/data/web_static/current"
+
+#give ownership to ubuntu user And Group
+sudo chown -R ubuntu:ubuntu "/data"
+
+#Update the Nginx configuration to serve the content 
+sudo sed -i "s/^.*location \/hbtn_static.*//" /etc/nginx/sites-available/default
+sudo sed -i \
+	"s/^}$/\tlocation \/hbtn_static \{ alias \/data\/web_static\/current\/; \}\n\}/" \
+	/etc/nginx/sites-available/default
+
+#enable default site
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+
+#restart Ngnix server
+sudo service nginx restart
