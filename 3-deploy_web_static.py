14277bb8fb386146f3f7a0b3f6aa864cc30c831d
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-10-02 18:39:47 +0100

    A Fabric Script that distributes an archive to my servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,61 +1,63 @@
 #!/usr/bin/python3
 # A Fabric Script that distributes an archive to my servers
-import os
 from datetime import datetime
-from fabric.api import local, env, run, put, sudo
+from fabric.api import *
+import os
+import shlex
 
 
-env.hosts = ['35.227.39.114', '35.231.236.123']
-
-
-def do_pack():
-    """Pack web static directory"""
-    dt = datetime.now()
-    dt_str = dt.strftime('%Y%m%d%H%M%S')
-    local('mkdir -p versions')
-    path = 'versions/web_static_' + dt_str + '.tgz'
-    tar = 'tar -cvzf {} web_static'.format(path)
-
-    if local(tar).failed:
-        return None
-    return path
-
-
-def do_deploy(archive_path):
-    """Deploy archive to servers"""
-    if not os.path.isfile(archive_path):
-        return False
-
-    archive_file = archive_path.split('/')[1]
-    archive_dir = archive_file.split('.')[0]
-    releases = '/data/web_static/releases/'
-    current = '/data/web_static/current'
-
-    if put(archive_path, '/tmp/').failed:
-        return False
-    if run('mkdir -p {}{}'.format(releases, archive_dir)).failed:
-        return False
-    if run('tar -xzf /tmp/{} -C {}{}'.format(
-            archive_file, releases, archive_dir)).failed:
-        return False
-    if run('rm /tmp/{}'.format(archive_file)).failed:
-        return False
-    if run('mv {}{}/web_static/* {}{}'.format(
-            releases, archive_dir, releases, archive_dir)).failed:
-        return False
-    if run('rm -rf {}{}/web_static'.format(releases, archive_dir)).failed:
-        return False
-    if run('rm -rf {}'.format(current)).failed:
-        return False
-    if run('ln -s {}{} {}'.format(releases, archive_dir, current)).failed:
-        return False
-
-    return True
+env.hosts = ['35.231.33.237', '34.74.155.163']
+env.user = "ubuntu"
 
 
 def deploy():
-    """Launches deploy processes"""
-    archive_path = do_pack()
-    if not archive_path:
+    """ DEPLOYS """
+    try:
+        archive_path = do_pack()
+    except:
         return False
+
     return do_deploy(archive_path)
+
+
+def do_pack():
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    except:
+        return False
