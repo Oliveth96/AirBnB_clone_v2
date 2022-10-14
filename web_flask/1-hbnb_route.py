36e63b2a0aabde133122ace27822b6e9e443fc2d
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-10-14 13:38:59 +0100

    A script that starts a Flask web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,7 +11,7 @@ def hello_Flask():
     return "Hello HBNB!"
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     """ Returns HBNB"""
     return "HBNB"
