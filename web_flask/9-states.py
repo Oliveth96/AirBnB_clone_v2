5f5029e5e5ec3c9b158a92ae415dc21f48abb7bb
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-10-14 16:36:59 +0100

    List of citites within a state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -7,11 +7,26 @@ from models.state import State
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def list_states_cities():
-    """ displays a list of all the cities within a state"""
+@app.route('/states', strict_slashes=False)
+def list_states():
+    """view that lists all of the states"""
+    id = None
     states = models.storage.all(State).values()
-    return render_template('9-states.html', states=states)
+    return render_template('9-states.html', states=states, id=id)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def list_state_cities(id=None):
+    """view that lists all cities by their states"""
+    found = False
+    states = models.storage.all(State).values()
+    for state in states:
+        if state.id == id:
+            found = True
+            states = [state]
+    if found is False:
+        states = []
+    return render_template('9-states.html', states=states, id=id)
 
 
 @app.teardown_appcontext
